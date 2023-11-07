2548195607d4c25e40d5ec8740c8f4e77f3b7c5c
Eric Tuovila <36007317+only-devices@users.noreply.github.com>
2023-11-07 07:17:25 -0500

    Modal shenanigans

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
-import "bootstrap";
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
 
 // More info for using bootstrap via npm:
 // https://getbootstrap.com/docs/5.0/getting-started/download/#npm
